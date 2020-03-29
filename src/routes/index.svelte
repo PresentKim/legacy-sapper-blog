71e1090fae853936e9d013319e7174e1d108c4ee
PresentKim <debe3721@gmail.com>
2020-03-29 23:59:20 +0900

    Update main page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,5 @@
 <style>
-	h1, figure, p {
+	h1{
 		text-align: center;
 		margin: 0 auto;
 	}
@@ -11,20 +11,6 @@
 		margin: 0 0 0.5em 0;
 	}
 
-	figure {
-		margin: 0 0 1em 0;
-	}
-
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
-
-	p {
-		margin: 1em auto;
-	}
-
 	@media (min-width: 480px) {
 		h1 {
 			font-size: 4em;
@@ -33,14 +19,7 @@
 </style>
 
 <svelte:head>
-	<title>Sapper project template</title>
+	<title>Blugin - MC:BE plugin dev team</title>
 </svelte:head>
 
-<h1>Great success!</h1>
-
-<figure>
-	<img alt='Borat' src='great-success.png'>
-	<figcaption>HIGH FIVE!</figcaption>
-</figure>
-
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+<h1>Make plugin for MC:BE server software</h1>
