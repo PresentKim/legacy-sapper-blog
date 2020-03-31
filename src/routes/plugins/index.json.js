c70d5ff030b18bf645a67d13717c52f2b948bed3
PresentKim <debe3721@gmail.com>
2020-03-31 22:20:48 +0900

    Add index.json.js (write response of plugins.json)

---
+++ src/routes/plugins/index.json.js
@@ -0,0 +1,11 @@
+import plugins from './_plugins';
+
+const contents = JSON.stringify(plugins);
+
+export function get(req, res) {
+	res.writeHead(200, {
+		'Content-Type': 'application/json'
+	});
+
+	res.end(contents);
+}
